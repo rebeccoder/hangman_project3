3b12aa2ba48023a5ec2f4dd37a915256540d9c44
rebeccoder <rebsoptay@icloud.com>
2023-03-03 23:25:21 +0000

    defined a function to select a word from words.txt file

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
 import random
+
+def new_word():
+    """
+    Randomly selects a word from words.txt file for each round
+    """
+    with open('words.txt', 'r') as h:
+        words = h.readlines()
+    random_word = random.choice(words)[:-1].upper()
+    return random_word
