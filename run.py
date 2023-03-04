8584df60d84cbf15aa0c9d7a0fd7424fabff3108
rebeccoder <rebsoptay@icloud.com>
2023-03-04 00:11:27 +0000

    started working on the play function, it now displays a secret word and amount of lives and promts the user to input a letter and returns whether its in the word or not and if it is then it adds the letter through a while loop and a if and else statement'

--- run.py
+++ run.py
@@ -8,3 +8,34 @@ def new_word():
         words = h.readlines()
     random_word = random.choice(words)[:-1].upper()
     return random_word
+
+def play(word):
+    """
+    Starts the game, displays the secret word, the picture of the hangman and the amount of lives.
+    """
+    word = new_word()
+    secret_word = "_" * len(word)
+    end_game = False
+    guessed_letters = []
+    lives = 6
+    print("Let's play Hangman!")
+    print(display_hangman(lives))
+    print("Guess this word: " + " ".join(secret_word) + "\n")
+    print("\n")
+    """
+    Promts for the user to input a letter and returns whether 
+    the letter is in the word or not, if it is then it adds the letter
+    to the word
+    """
+    while not end_game and lives >= 0:
+        guess = input("Please try a letter").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print(f"You have already tried the letter {guess}!")
+            elif guess in word:
+                print(f"{guess} is not in the word!")
+        else:
+            print(f"{guess} is in the word! Well done!")
+            guessed_letters.append(guess)
+
+
