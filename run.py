ab789fb5ab06a00a468fc76c8e604e9d2463987c
rebeccoder <rebsoptay@icloud.com>
2023-03-04 15:04:07 +0000

    added else if statement for whether the letter the user tries is in the word or not

--- run.py
+++ run.py
@@ -9,11 +9,13 @@ def new_word():
     random_word = random.choice(words)[:-1].upper()
     return random_word
 
+word = new_word()
+
 def play(word):
     """
     Starts the game, displays the secret word, the picture of the hangman and the amount of lives.
     """
-    word = new_word()
+
     secret_word = "_" * len(word)
     end_game = False
     guessed_letters = []
@@ -25,7 +27,7 @@ def play(word):
     """
     Promts for the user to input a letter and returns whether 
     the letter is in the word or not, if it is then it adds the letter
-    to the word
+    to the word, if the word is completed then the game is ended
     """
     while not end_game and lives >= 0:
         guess = input("Please try a letter").upper()
@@ -33,9 +35,16 @@ def play(word):
             if guess in guessed_letters:
                 print(f"You have already tried the letter {guess}!")
             elif guess in word:
-                print(f"{guess} is not in the word!")
+                print(f"Uh oh{guess} is not in the word!")
+                lives -= 1
+                guessed_letters.append(guess)
         else:
             print(f"{guess} is in the word! Well done!")
             guessed_letters.append(guess)
-
-
+            word_as_list = list(secret_word)
+            indices = [i for i, letter in enumerate(word) if letter == guess]
+            for index in indices:
+                word_as_list[index] = guess
+            secret_word = "".join(word_as_list)
+            if "_" not in secret_word:
+                end_game = True
