7e9e2c6c237314c10004390041762c10efab1442
rebeccoder <rebsoptay@icloud.com>
2023-03-06 15:22:49 +0000

    removed the = in 32 as it was the cause of a bug in the programme

--- run.py
+++ run.py
@@ -29,7 +29,7 @@ def play(word):
     the letter is in the word or not, if it is then it adds the letter
     to the word, if the word is completed then the winning/losing message is shown
     """
-    while not end_game and lives >= 0:
+    while not end_game and lives > 0:
         guess = input("Please try a letter ").upper()
         if len(guess) == 1 and guess.isalpha():
             if guess in guessed_letters:
